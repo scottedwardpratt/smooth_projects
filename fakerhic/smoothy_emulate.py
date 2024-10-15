3ea5759de495d4947b570cf072f58a176fde2327
scottpratt <prattsc@msu.edu>
2024-10-15 09:35:45 -0400

    yada yada

--- fakerhic/smoothy_emulate.py
+++ fakerhic/smoothy_emulate.py
@@ -4,17 +4,28 @@ sys.path.insert(0,"/Users/scottpratt/git/smooth_projects/mylocal/software/pybind
 import emulator_smooth
 smoothmaster=emulator_smooth.emulator_smooth()
 
+smoothmaster.TuneAllY()
+print('tuning completed')
+
 NPars=smoothmaster.GetNPars()
 NObs=smoothmaster.GetNObs()
 
 theta=np.zeros(NPars,dtype='float')
+X=np.zeros(NPars,dtype='float')
+
+X[0]=210.0
+X[1]=0.1715
+X[2]=0.75
+X[3]=0.55
+X[4]=1.4
+X[5]=22.5
+
+print('X=',X)
+theta=smoothmaster.GetThetaFromX(X)
+print('theta=',theta)
+
 Y=np.zeros(NObs,dtype='float')
 SigmaY=np.zeros(NObs,dtype='float')
-
-smoothmaster.TuneAllY()
-print('tuning completed')
-
-print('theta=',theta)
 
 for iY in range(0,NObs):
   #Y[iY]=a.GetYOnlyPython(iY,theta)
