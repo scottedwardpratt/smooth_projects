475dbeb598856c04de2ed2c6f4b5b62d2af93361
scottpratt <prattsc@msu.edu>
2025-01-06 17:16:53 -0500

    yada yada

--- mylocal/software/main_programs/smoothy_vslambda_main.cc
+++ mylocal/software/main_programs/smoothy_vslambda_main.cc
@@ -5,18 +5,19 @@
 using namespace std;
 int main(){
 	NBandSmooth::CSmoothMaster master;
-	double Lambda,sigmaAbar,logPbar,logP,sigma2,dsigma,sigmaA;
-	double sig1,sig2,sig3;
+	double Lambda,sigmaAbar,Lambdabar,LambdaGuess,logPbar,logP,sigma2,dsigma,sigmaA;
 	unsigned int iY,NObs,isigma,Nsigma=100;
 	vector<unsigned int> sigmadist(Nsigma);
 	string filename;
-	FILE *fptr1,*fptr2,*fptr3;
+	FILE *fptr1,*fptr2;//*fptr3;
 	dsigma=5.0;
 	
 	NObs=master.observableinfo->NObservables;
 
 	fptr2=fopen("SigmaVsLambda/SigmaAvslambda.txt","w");
-	for(Lambda=2;Lambda<8.01;Lambda+=1){
+	//printf("Enter Lambda: ");
+	//scanf("%lf",&Lambda);
+	for(Lambda=1.5;Lambda<6.501;Lambda+=0.5){
 		for(isigma=0;isigma<Nsigma;isigma++)
 			sigmadist[isigma]=0;
 		std::string Lstr = std::to_string (Lambda);
@@ -24,10 +25,10 @@ int main(){
 		Lstr.erase ( Lstr.find_last_not_of('.') + 1, std::string::npos );
 		filename="SigmaVsLambda/sigmadist_Lambda"+Lstr+".txt";
 		fptr1=fopen(filename.c_str(),"w");
-		filename="SigmaVsLambda/sig123_"+Lstr+".txt";
-		fptr3=fopen(filename.c_str(),"w");
+		//filename="SigmaVsLambda/sig123_"+Lstr+".txt";
+		//fptr3=fopen(filename.c_str(),"w");
 		
-		sigmaAbar=sigma2=logPbar=0.0;
+		sigmaAbar=sigma2=logPbar=Lambdabar=0.0;
 		for(iY=0;iY<NObs;iY++){
 			master.emulator[iY]->LAMBDA=Lambda;
 			master.emulator[iY]->Bcalculated=false;
@@ -37,29 +38,32 @@ int main(){
 		fprintf(fptr1,"#Lambda=%5.2f, dsigma=%g\n",Lambda,dsigma);
 		fprintf(fptr1,"#Sigma   Ndist(Sigma)\n");
 		for(iY=0;iY<NObs;iY++){
-			master.emulator[iY]->CalcExactLogP();
+			//master.emulator[iY]->CalcSigmaLambda(LambdaGuess);
 			sigmaA=master.emulator[iY]->SigmaA;
-			master.emulator[iY]->GetSigmaA123(sig1,sig2,sig3);
+			master.emulator[iY]->CalcExactLogP();
+			LambdaGuess=Lambda;
 			logP=master.emulator[iY]->logP;
 			sigmaAbar+=sigmaA;
 			logPbar+=logP;
 			sigma2+=sigmaA*sigmaA;
+			Lambdabar+=LambdaGuess;
 			isigma=lrint(sigmaA/dsigma);
 			if(isigma<Nsigma){
 				sigmadist[isigma]+=1;
 			}
-			fprintf(fptr3,"%8.3f %8.3f %8.3f\n",sig1,sig2,sig3);
+			//fprintf(fptr3,"%8.3f %8.3f %8.3f\n",sig1,sig2,sig3);
 		}
 		sigmaAbar=sigmaAbar/double(NObs);
-		printf("%4d: <sigmaA>=%g\n",iY,sigmaAbar);
+		Lambdabar=Lambdabar/double(NObs);
 		logPbar=logPbar/double(NObs);
+		printf("%g: <sigmaA>=%g  <Lambda>=%g <logP>=%g\n",Lambda,sigmaAbar,Lambdabar,logPbar);
 		sigma2=sigma2/double(NObs)-sigmaAbar*sigmaAbar;
 		for(isigma=0;isigma<Nsigma;isigma++){
 			fprintf(fptr1,"%6.1f %u\n",isigma*dsigma,sigmadist[isigma]);
 		}
-		fprintf(fptr2,"%8.2f %8.2f %8.2f %8.2f\n",Lambda,logPbar,sigmaAbar,sqrt(sigma2));
+		fprintf(fptr2,"%8.2f %8.2f %8.2f %8.2f\n",Lambdabar,logPbar,sigmaAbar,sqrt(sigma2));
 		fclose(fptr1);
-		fclose(fptr3);
+		//fclose(fptr3);
 	}
 	fclose(fptr2);
 	
