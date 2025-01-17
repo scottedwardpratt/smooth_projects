0552489f4fd9ba095bd5e06549f4175732778b5f
scottpratt <prattsc@msu.edu>
2025-01-17 05:02:37 -0500

    yada yada

--- mylocal/software/fakefullmodels/fakesmooth.cc
+++ mylocal/software/fakefullmodels/fakesmooth.cc
@@ -7,26 +7,27 @@
 using namespace std;
 using namespace NMSUUtils;
 int main(){
+	bool gu=false;
 	double GSCALE=sqrt(3.0);
-	double sigmax=100.0,ALPHA;
+	double ALPHA;
 	char dummy[400];
-	char gu;
-	bool gaussian=true;
 	unsigned int NObs,NPars;
-	unsigned int NTrain,itrain,ic,ipar,maxrank=5,iobs;
+	unsigned int NTrain,itrain,ic,ipar,maxrank=5,iy;
 	double LAMBDA,y;
 	CparameterMap parmap;
 	vector<vector<double>> A;
-	vector<double> theta,exptheta,X,SigmaY,xtrain,Ytrain;
+	vector<vector<double>> xtrain, thetatrain;
+	vector<double> theta,exptheta,SigmaY,Ytrain;
 	vector<string> priortype;
 	string expfilename,filename,command;
 	vector<string> obsname;
 	vector<string> parname;
-	vector<double> xmin,xmax,thetatrue,ytrue;
+	vector<double> xmin,xmax,x0,Rgauss,thetatrue,ytrue;
+	char parname_c[200],type[100],typeread[10];
 	FILE *fptr;
 	NMSUUtils::Crandy randy(-time(NULL));
 	//NMSUUtils::Crandy randy(-123);
-	char parname_c[200],type[100];
+
 	parmap.ReadParsFromFile("smooth_data/smooth_parameters/simplex_parameters.txt");
 	parmap.ReadParsFromFile("smooth_data/smooth_parameters/emulator_parameters.txt");
 	fptr=fopen("NParsNObs.txt","r");
@@ -36,15 +37,26 @@ int main(){
 	
 	printf("Enter Lambda for fakesmooth: ");
 	scanf("%lf",&LAMBDA);
-	printf("For testing points: Enter g for gaussian, u for uniform: ");
-	scanf(" %c",&gu);
-	//LAMBDA=4.0;
-	//gu='g';
-	if(gu=='g')
-		gaussian=true;
+	printf("Enter u or g for gaussian/uniform prior: ");
+	scanf("%s",typeread);
+	if(typeread[0]=='u')
+		gu=false;
+	else if(typeread[0]=='g')
+		gu=true;
 	else
-		gaussian=false;
-	
+		CLog::Fatal("must enter g or u\n");
+
+	thetatrue.resize(NPars);
+	ytrue.resize(NObs);
+	SigmaY.resize(NObs);
+	xmin.resize(NPars);
+	xmax.resize(NPars);
+	x0.resize(NPars);
+	Rgauss.resize(NPars);
+	obsname.resize(NObs);
+	Ytrain.resize(NObs);
+
+	// Find NTrain
 	NTrain=0;
 	bool existence;
 	do{
@@ -55,33 +67,31 @@ int main(){
 			NTrain+=1;
 		}
 	}while(existence);
+	xtrain.resize(NTrain);
+	thetatrain.resize(NTrain);
+	for(itrain=0;itrain<NTrain;itrain++){
+		xtrain[itrain].resize(NPars);
+		thetatrain[itrain].resize(NPars);
+	}
 	//CLog::Info("NTraining Pts="+to_string(NTrain)+"\n");
+
+	// Create smooth object
 	NBandSmooth::CSmooth smooth(NPars,maxrank);
 	
-	thetatrue.resize(NPars);
-	theta.resize(NPars);
-	ytrue.resize(NObs);
-	SigmaY.resize(NObs);
-	xmin.resize(NPars);
-	xmax.resize(NPars);
-	X.resize(NPars);
-	obsname.resize(NObs);
-	
-	xtrain.resize(NPars);
-	Ytrain.resize(NObs);
-	
+
+	// Create A for smooth object
 	A.resize(NObs);
-	for(iobs=0;iobs<NObs;iobs++){
-		A[iobs].resize(smooth.NCoefficients);
+	for(iy=0;iy<NObs;iy++){
+		A[iy].resize(smooth.NCoefficients);
 		for(ic=0;ic<smooth.NCoefficients;ic++){
-			A[iobs][ic]=100.0*randy.ran_gauss();
+			A[iy][ic]=100.0*randy.ran_gauss();
 		}
 	}
 	
 	// Observable uncertainties
-	for(iobs=0;iobs<NObs;iobs++){
-		SigmaY[iobs]=5.0;
-		obsname[iobs]="obs"+to_string(iobs);
+	for(iy=0;iy<NObs;iy++){
+		SigmaY[iy]=5.0;
+		obsname[iy]="obs"+to_string(iy);
 	}
 	
 	// read in modelpar_info and set experimental value to theta=0.2
@@ -89,20 +99,23 @@ int main(){
 	fgets(dummy,400,fptr);
 	for(ipar=0;ipar<NPars;ipar++){
 		fscanf(fptr,"%s %s %lf %lf",parname_c,type,&xmin[ipar],&xmax[ipar]);
+		if(gu){
+			x0[ipar]=xmin[ipar];
+			Rgauss[ipar]=xmax[ipar];
+		}
 		parname.push_back(string(parname_c));
 		thetatrue[ipar]=0.2;
 	}
 	fclose(fptr);
 	fptr=fopen("smooth_data/Info/experimental_info.txt","w");
-	for(iobs=0;iobs<NObs;iobs++){
-		ytrue[iobs]=smooth.CalcY(A[iobs],LAMBDA,thetatrue);
+	for(iy=0;iy<NObs;iy++){
+		ytrue[iy]=smooth.CalcY(A[iy],LAMBDA,thetatrue);
 		fprintf(fptr,"%s\t%g\t%g 0.0\n",
-		obsname[iobs].c_str(),ytrue[iobs],SigmaY[iobs]);
+		obsname[iy].c_str(),ytrue[iy],SigmaY[iy]);
 	}
 	fclose(fptr);
 	
-	// write output at training pts
-	
+	// write observables at training pts
 	FILE *fptr_thetas=fopen("SigmaVsLambda/TrainingThetas.txt","w");
 	FILE *fptr_obs=fopen("SigmaVsLambda/TrainingObs.txt","w");
 	FILE *fptr_SigmaY=fopen("SigmaVsLambda/TrainingSigmaY.txt","w");
@@ -111,98 +124,68 @@ int main(){
 		filename="smooth_data/modelruns/run"+to_string(itrain)+"/mod_parameters.txt";
 		fptr=fopen(filename.c_str(),"r");
 		for(ipar=0;ipar<NPars;ipar++){
-			fscanf(fptr,"%s %lf",parname_c,&xtrain[ipar]);
-			fprintf(fptr_thetas,"%15.8f ",xtrain[ipar]);
-			theta[ipar]=-1.0+2.0*(xtrain[ipar]-xmin[ipar])/(xmax[ipar]-xmin[ipar]);
+			fscanf(fptr,"%s %lf",parname_c,&xtrain[itrain][ipar]);
+			fprintf(fptr_thetas,"%15.8f ",xtrain[itrain][ipar]);
+			if(gu){
+				thetatrain[itrain][ipar]=(xtrain[itrain][ipar]-x0[ipar])/(Rgauss[ipar]*GSCALE);
+			}
+			else{
+				thetatrain[itrain][ipar]=-1.0+2.0*(xtrain[itrain][ipar]-xmin[ipar])/(xmax[ipar]-xmin[ipar]);
+			}
 		}
 		fprintf(fptr_thetas,"\n");
-		fclose(fptr);		
-		
+		fclose(fptr);	
+
+
 		filename="smooth_data/modelruns/run"+to_string(itrain)+"/obs.txt";
 		fptr=fopen(filename.c_str(),"w");
-		
-		for(iobs=0;iobs<NObs;iobs++){
-			y=smooth.CalcY(A[iobs],LAMBDA,theta);
+		for(iy=0;iy<NObs;iy++){
+			Ytrain[iy]=smooth.CalcY(A[iy],LAMBDA,thetatrain[itrain]);
 
 			double randomerror=0.0;
-			fprintf(fptr,"%s %lf %lf\n",obsname[iobs].c_str(),y,randomerror);
-			fprintf(fptr_obs,"%15.8f ",y);
-			fprintf(fptr_SigmaY,"%15.8f ",SigmaY[iobs]);
+			fprintf(fptr,"%s %lf %lf\n",obsname[iy].c_str(),Ytrain[iy],randomerror);
+			fprintf(fptr_obs,"%15.8f ",Ytrain[iy]);
+			fprintf(fptr_SigmaY,"%15.8f ",SigmaY[iy]);
 		}
 		fclose(fptr);
 		fprintf(fptr_obs,"\n");
 		fprintf(fptr_SigmaY,"\n");
-		
-		
 	}
 	fclose(fptr_thetas);
 	fclose(fptr_obs);
 	fclose(fptr_SigmaY);
-	
 	//
 	
 	// Now make some data for later testing, not for tuning
-	FILE *expfptr;
-	int iy,it=time(NULL);
-	double x;
-	vector<vector<double>> thetatest;
+	vector<vector<double>> thetatest,xtest;
 	int itest,Ntest=50;
-	FILE *fptr_out;
 	
 	thetatest.resize(Ntest);
+	xtest.resize(Ntest);
 	for(itest=0;itest<Ntest;itest++){
 		thetatest[itest].resize(NPars);
+		xtest[itest].resize(NPars);
 		for(ipar=0;ipar<NPars;ipar++){
-			if(gaussian)
-				thetatest[itest][ipar]=(1.0/GSCALE)*randy.ran_gauss();
-			else
+			if(gu){
 				thetatest[itest][ipar]=-1.0+2.0*randy.ran();
+			}
+			else{
+				thetatest[itest][ipar]=randy.ran_gauss()/GSCALE;
+			}
+			if(gu){
+				xtest[itest][ipar]=x0[ipar]+Rgauss[ipar]*thetatest[itest][ipar]*GSCALE;
+			}
+			else{
+				xtest[itest][ipar]=xmin[ipar]
+				+0.5*(1.0+thetatest[itest][ipar])*(xmax[ipar]-xmin[ipar]);
+			}
+
 		}
 	}
 	
 	command="mkdir -p smooth_data/fullmodel_testdata";
 	system(command.c_str());
-	theta.resize(NPars);
-	A.resize(NObs);
-	expfilename="smooth_data/Info/experimental_info.txt";
-	expfptr=fopen(expfilename.c_str(),"w");
-	exptheta.resize(NPars);
-	for(ipar=0;ipar<NPars;ipar++){
-		exptheta[ipar]=0.2;
-	}
-	
-	for(iy=0;iy<int(NObs);iy++){
-		A[iy].resize(NPars);
-		randy.reset(iy+it);
-		for(ic=0;ic<smooth.NCoefficients;ic++){
-			A[iy][ic]=100.0*randy.ran_gauss();
-		}
-		for(itrain=0;itrain<NTrain;itrain++){
-			filename="smooth_data/modelruns/run"+to_string(itrain)+"/mod_parameters.txt";
-			fptr=fopen(filename.c_str(),"r");
-			for(ipar=0;ipar<NPars;ipar++){
-				fscanf(fptr,"%s %lf",parname_c,&x);
-				parname[ipar]=parname_c;
-				if(gaussian){
-					double xbar=0.0;
-					theta[ipar]=(x-xbar)/(sigmax*GSCALE);
-				}
-				else
-					theta[ipar]=-1.0+2.0*(x-xmin[ipar])/(xmax[ipar]-xmin[ipar]);
-			}
-			fclose(fptr);
-			
-			y=smooth.CalcY(A[iy],LAMBDA,theta);
-			filename="smooth_data/modelruns/run"+to_string(itrain)+"/obs.txt";
-			double randomerror=0.0;
-			if(iy==0)
-				fptr_out=fopen(filename.c_str(),"w");
-			else
-				fptr_out=fopen(filename.c_str(),"a");
-			fprintf(fptr_out,"%s %15.8f %15.8f\n",obsname[iy].c_str(),y,randomerror);
-			fclose(fptr_out);
-		}
-
+	for(iy=0;iy<NObs;iy++){
 		filename="smooth_data/fullmodel_testdata/"+obsname[iy]+".txt";
 		fptr=fopen(filename.c_str(),"w");
 		for(itest=0;itest<Ntest;itest++){
@@ -214,6 +197,17 @@ int main(){
 		}
 		fclose(fptr);
 		
+	}
+
+	// write experimental data (for use with mcmc)
+	FILE *expfptr;
+	expfilename="smooth_data/Info/experimental_info.txt";
+	expfptr=fopen(expfilename.c_str(),"w");
+	exptheta.resize(NPars);
+	for(ipar=0;ipar<NPars;ipar++){
+		exptheta[ipar]=0.2;
+	}
+	for(iy=0;iy<NObs;iy++){
 		y=smooth.CalcY(A[iy],LAMBDA,exptheta);
 		fprintf(expfptr,"%s  %lf  3.0 0.0\n",obsname[iy].c_str(),y);
 	}
