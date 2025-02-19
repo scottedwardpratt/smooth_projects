6afeedec42abf94f761216d516f324b277f88ead
scottpratt <prattsc@msu.edu>
2025-02-19 18:53:29 -0500

    yada yada

--- mylocal/software/main_programs/simplex_optimize_main.cc
+++ mylocal/software/main_programs/simplex_optimize_main.cc
@@ -6,10 +6,15 @@ using namespace std;
 using namespace NMSUUtils;
 int main(){
 	double LAMBDA,ALPHA;
+	int NTrain;
 	NBandSmooth::CSimplexSampler *simplex=new NBandSmooth::CSimplexSampler();
-	printf("Enter Estimate for LAMBDA and ALPHA: ");
-	scanf("%lf %lf",&LAMBDA,&ALPHA);
+	FILE *fptr=fopen("LambdaAlpha.txt","r");
+	//printf("Enter Estimate for LAMBDA and ALPHA: ");
+	//scanf("%lf %lf",&LAMBDA,&ALPHA);
+	fscanf(fptr,"%lf %lf %d",&LAMBDA,&ALPHA,&NTrain);
+	simplex->NTrainingPts=NTrain;
 	simplex->Optimize(LAMBDA,ALPHA);
-	simplex->WriteModelPars();
+	//simplex->WriteModelPars();
+	fclose(fptr);
 	return 0;
 }
