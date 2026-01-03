8b2b45f9f5c32aa50730902e605b3cd96bf0fbae
scottpratt <prattsc@msu.edu>
2026-01-02 22:54:20 -0500

    yada yada

--- mylocal/software/MainPrograms/smoothy_testattrainingpts_main.cc
+++ mylocal/software/MainPrograms/smoothy_testattrainingpts_main.cc
@@ -5,7 +5,9 @@
 using namespace std;
 int main(){
 	NBandSmooth::CSmoothMaster master;
+   printf("master created\n");
 	master.TuneAllY();
+   printf("tuning finished\n");
 	master.TestAtTrainingPts();
 	return 0;
 }
