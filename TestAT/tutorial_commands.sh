b7ffc56b6f597b05c3733da4f8f5e5fbdaed6e43
scottpratt <prattsc@msu.edu>
2026-01-04 11:40:27 -0500

    yada yada

--- TestAT/tutorial_commands.sh
+++
@@ -1,9 +0,0 @@
-make -C ../mylocal/software
-rm -f -r smooth_data/FullModelRuns/run*
-rm -f -r smooth_data/FullModelTestingRuns/run*
-rm -f -r smooth_data/fullmodel_testdata/*
-fakeinfo;
-trainingpoint_optimizer;
-fakefullmodel;
-smoothy_testattrainingpts
-smoothy_testvsfullmodel
